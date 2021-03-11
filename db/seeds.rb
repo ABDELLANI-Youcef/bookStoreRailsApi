5af34dacacc4ab7d91b9863a167611c922c4689d
wrack <carlosveiga1618@gmail.com>
2021-03-11 12:40:03 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: 'Book 1 A', author: 'Carlos', category: 'Action')
+Book.create(title: 'Book 1 B', author: 'Youcef', category: 'Biography')
+Book.create(title: 'Book 1 H', author: 'Carlos', category: 'History')
+Book.create(title: 'Book 1 Hor', author: 'Youcef', category: 'Horror')
+Book.create(title: 'Book 1 K', author: 'Carlos', category: 'Kids')
+Book.create(title: 'Book 1 L', author: 'Youcef', category: 'Learning')
+Book.create(title: 'Book 1 SF', author: 'Carlos', category: 'Sci-Fi')
+
